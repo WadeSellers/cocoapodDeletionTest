2215b8116691c07e8f987d8f6f075a60801eeba4
Wade Sellers <Waderoni@gmail.com>
2015-01-28 08:59:02 -0700

    Initial Commit

---
+++ cocoapodDeletionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoapodDeletionTest
+//
+//  Created by Wade Sellers on 1/28/15.
+//  Copyright (c) 2015 WadeSellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
